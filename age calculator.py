4b257e8c2fc6561a32d9ee589677db1b5e48f6aa
anikettalekar <100402324+anikettalekar@users.noreply.github.com>
2022-05-09 21:06:06 +0530

    Add files via upload

---
+++ age calculator.py
@@ -0,0 +1,10 @@
+def ageCalculator(y, m, d):
+    import datetime
+    today = datetime.datetime.now().date()
+    dob = datetime.date(y, m, d)
+    age = int((today-dob).days / 365.25)
+    print(age,"Years Old")
+y=int(input("Enter Your Birth Year : "))
+m=int(input("Enter Your Month of Birth : "))
+d=int(input("Enter Your Date of Birth : "))
+ageCalculator(y,m,d)
